516e05360d4dd7d3968d442e0e41a8e78de1a72d
prasun <prasunanand.bitsp@gmail.com>
2017-05-16 12:06:47 +0530

    implemented matmul example for floats

--- source/matmul.d
+++ source/matmul.d
@@ -7,14 +7,15 @@ import cuda_d.cublas;
 import cuda_d.cublas_api;
 import cuda_d.cublas_v2;
 import cuda_d.cuda_runtime_api;
+import cuda_d.curand;
 
 void GPU_fill_rand(float *A, int nr_rows_A, int nr_cols_A) {
   // Create a pseudo-random number generator
   curandGenerator_t prng;
-  curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
+  curandCreateGenerator(&prng, curandRngType.CURAND_RNG_PSEUDO_DEFAULT);
 
   // Set the seed for the random number generator using the system clock
-  curandSetPseudoRandomGeneratorSeed(prng, cast(ulong)clock());
+  curandSetPseudoRandomGeneratorSeed(prng, 4);
 
   // Fill the array with random numbers on the device
   curandGenerateUniform(prng, A, nr_rows_A * nr_cols_A);
@@ -32,7 +33,7 @@ void gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const 
   cublasCreate(&handle);
 
   // Do the actual multiplication
-  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+  cublasSgemm(handle, cublasOperation_t.CUBLAS_OP_N, cublasOperation_t.CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 
   // Destroy the handle
   cublasDestroy(handle);
@@ -56,36 +57,36 @@ void call_gemm_routine(){
   // for simplicity we are going to use square arrays
   nr_rows_A = nr_cols_A = nr_rows_B = nr_cols_B = nr_rows_C = nr_cols_C = 3;
 
-  auto h_A = new double[nr_rows_A * nr_cols_A];
-  auto h_B = new double[nr_rows_B * nr_cols_B];
-  auto h_C = new double[nr_rows_C * nr_cols_C];
+  auto h_A = new float[nr_rows_A * nr_cols_A];
+  auto h_B = new float[nr_rows_B * nr_cols_B];
+  auto h_C = new float[nr_rows_C * nr_cols_C];
 
   // Allocate 3 arrays on GPU
-  double* d_A, d_B, d_C;
-  cudaMalloc(cast(void **)&d_A,nr_rows_A * nr_cols_A * cast(int)double.sizeof);
-  cudaMalloc(cast(void **)d_B,nr_rows_B * nr_cols_B * cast(int)double.sizeof);
-  cudaMalloc(cast(void **)d_C,nr_rows_C * nr_cols_C * cast(int)double.sizeof);
+  float* d_A, d_B, d_C;
+  cudaMalloc(cast(void **)&d_A,nr_rows_A * nr_cols_A * cast(int)float.sizeof);
+  cudaMalloc(cast(void **)&d_B,nr_rows_B * nr_cols_B * cast(int)float.sizeof);
+  cudaMalloc(cast(void **)&d_C,nr_rows_C * nr_cols_C * cast(int)float.sizeof);
 
   // Fill the arrays A and B on GPU with random numbers
   GPU_fill_rand(d_A, nr_rows_A, nr_cols_A);
   GPU_fill_rand(d_B, nr_rows_B, nr_cols_B);
 
   // Optionally we can copy the data back on CPU and print the arrays
-  cudaMemcpy(h_A,d_A,nr_rows_A * nr_cols_A * cast(int)double.sizeof,cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_A.ptr,d_A,nr_rows_A * nr_cols_A * cast(int)float.sizeof, cudaMemcpyKind.cudaMemcpyDeviceToHost);
 
-  cudaMemcpy(h_B,d_B,nr_rows_B * nr_cols_B * cast(int)double.sizeof,cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_B.ptr,d_B,nr_rows_B * nr_cols_B * cast(int)float.sizeof, cudaMemcpyKind.cudaMemcpyDeviceToHost);
   writeln( "A =");
-  print_matrix(h_A, nr_rows_A, nr_cols_A);
+  print_matrix(h_A.ptr, nr_rows_A, nr_cols_A);
   writeln( "B =");
-  print_matrix(h_B, nr_rows_B, nr_cols_B);
+  print_matrix(h_B.ptr, nr_rows_B, nr_cols_B);
 
   // Multiply A and B on GPU
   gpu_blas_mmul(d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B);
 
   // Copy (and print) the result on host memory
-  cudaMemcpy(h_C,d_C,nr_rows_C * nr_cols_C * cast(int)double.sizeof,cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_C.ptr,d_C,nr_rows_C * nr_cols_C * cast(int)float.sizeof, cudaMemcpyKind.cudaMemcpyDeviceToHost);
   writeln( "C =" );
-  print_matrix(h_C, nr_rows_C, nr_cols_C);
+  print_matrix(h_C.ptr, nr_rows_C, nr_cols_C);
 
   //Free GPU memory
   cudaFree(d_A);
@@ -93,7 +94,7 @@ void call_gemm_routine(){
   cudaFree(d_C);
 
   // Free CPU memory
-  free(h_A);
-  free(h_B);
-  free(h_C);
+  //free(h_A);
+  //free(h_B);
+  //free(h_C);
 }
